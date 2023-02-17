885d3ac522561b1d769bac46b9e17da4f3925fec
arobin30 <arobin30@jaguarlandrover.com>
2023-02-17 10:02:43 +0000

    finish module

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -5,9 +5,9 @@ start_time = None
 total_written = 0
 
 @hooks.register("start_build")
-def strat_build():
+def start_build():
     global start_time
-    start_time = time.localtime()
+    start_time = time.time()
 
 @hooks.register("written")
 def written():
@@ -16,8 +16,7 @@ def written():
 
 @hooks.register("stats")
 def stats():
-    final_time = time.localtime() - start_time
-    if not total_written == 0:
-        average = final_time / total_written
+    final_time = time.time() - start_time
+    average = final_time / total_written if total_written else 0
     report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
     print(report.format(total_written, final_time, average))
